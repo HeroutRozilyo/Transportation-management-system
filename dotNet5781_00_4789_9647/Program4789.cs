aa89f2a8ffa97318cc3f6f23039d983798e7eb1a
da077 <da077@DESKTOP-HV19KNK>
2020-10-21 20:31:36 +0300

    welcome4789

---
+++ dotNet5781_00_4789_9647/Program4789.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_4789_9647
+{
+    partial class Program
+    {
+   
+
+        static void Main(string[] args)
+        {
+            Welcome4789();
+            Welcome9647();
+            Console.ReadKey();
+
+            
+        }
+       private static void Welcome4789()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", name);
+        }
+        static partial void Welcome9647();
+    }
+    
+}
