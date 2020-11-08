07a83b4efc9fbe739a6730dcb6cf92a9c4e6abc5
heroot12@gmail.com <heroot12@gmail.com>
2020-11-08 17:03:55 +0200

    ghf

--- dotNet5781_02_4789_9647/Properties/LineBus.cs
+++ dotNet5781_02_4789_9647/Properties/LineBus.cs
@@ -212,7 +212,7 @@ namespace dotNet5781_02_4789_9647.Properties
         public int CompareTo(object obj)
         {
             TimeSpan a=TravelLine(obj);
-            return a.CompareTo((/*(LineBus)obj).*/TravelLine(this));
+            return a.CompareTo(((LineBus)obj)TravelLine(this));
         }
     }
    
