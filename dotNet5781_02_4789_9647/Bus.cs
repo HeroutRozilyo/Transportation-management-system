d9db9502523a4907acbce6087378ca75447323fe
da077 <da077@DESKTOP-HV19KNK>
2020-11-08 17:28:30 +0200

    f

---
+++ dotNet5781_02_4789_9647/Bus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_4789_9647
+{
+    public class Bus
+    {
+        public int Mispar { get; set; }
+        public DateTime StartYear { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Bus {0} was klita be {1}", Mispar, StartYear.Year.ToString());
+        }
+    }
+}
