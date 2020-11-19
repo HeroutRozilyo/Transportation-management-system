5f6d030c9ff4249a42c678473a4ea163e9510b68
da077 <da077@DESKTOP-HV19KNK>
2020-11-19 12:49:00 +0200

    cs

--- doNEt5781_03A_4789_9647/MainWindow.xaml.cs
+++ doNEt5781_03A_4789_9647/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using dotNet5781_02_4789_9647.Properties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,16 +22,55 @@ namespace doNEt5781_03A_4789_9647
     /// </summary>
     public partial class MainWindow : Window
     {
-        static Random lineRandom= new Random(DataTime )
+        static Random lineRandom = new Random(DateTime.Now.Millisecond);
+        BusCompany egged = new BusCompany();
+        List<BusStation> stations = new List<BusStation>();
+            
+        
+
         public MainWindow()
         {
-           
+            for (int i = 0; i < 40; i++)//restart stations
+            {
+                string name = string.Format("station " + (i + 1));
+                stations.Add(new BusStation((i + 1), name));
+            }
+
+            for (int i = 0; i < 10; i++) //restart 10 lines with first station and last station. the constructor at lineBus random the number line.
+            {
+                egged.ComanyBus.Add(new LineBus(stations[i], stations[i + 1])); //now we restart 10 buses
+            }
+            for (int k = 1; k <= 3; k++)       //add stations to the lines
+            {
+
+                int j = 10 * k;
+                for (int i = 0; i < 10; i++)
+                {
+
+                    egged.ComanyBus[i].addAtLineBus(stations[j]);
+                    j++;
+                }
+            }
+
             InitializeComponent();
+            initComboBox();
+           
+
+        }
+        private LineBus currentDisplayBusLine;
+
+        private void initComboBox()
+        {
+            cbBusLines.ItemsSource = stations;
+            cbBusLines.DisplayMemberPath = " NumberID ";
+            cbBusLines.SelectedIndex = 0;
         }
 
         private void tbArea_TextChanged(object sender, TextChangedEventArgs e)
         {
+        }
 
+        
     }
 
 
