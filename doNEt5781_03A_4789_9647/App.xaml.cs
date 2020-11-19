71608693b405329115c2884c88caa942fb046205
heroot12@gmail.com <heroot12@gmail.com>
2020-11-19 09:35:32 +0200

    hhg

---
+++ doNEt5781_03A_4789_9647/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace doNEt5781_03A_4789_9647
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
