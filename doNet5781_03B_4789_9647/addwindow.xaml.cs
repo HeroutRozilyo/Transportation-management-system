8fa8b0d4d2663ecb51f52326f318bb32d5e39988
da077 <da077@DESKTOP-HV19KNK>
2020-12-08 18:16:03 +0200

    fff

--- doNet5781_03B_4789_9647/addwindow.xaml.cs
+++ doNet5781_03B_4789_9647/addwindow.xaml.cs
@@ -66,10 +66,14 @@ namespace doNet5781_03B_4789_9647
             {
                 myBus = new Bus
                 {
-                    License = this.licenseTextBox.Text
-                    // Fuel = int.Parse(this.fuelTextBox.Text)
-                   // StartingDate=DateTime.Prase(this.StartingDateTextBox.Text)
+
+                    License = this.licenseTextBox.Text,
+                    StartingDate = DateTime.Parse(this.lastTreatDatePicker.Text)
+
+
                 };
+
+
 
                 this.DialogResult = true;
                 this.Close();
