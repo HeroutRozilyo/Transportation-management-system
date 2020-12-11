4cb7858687931559303fa360bf8d2addc36ed48c
heroot12@gmail.com <heroot12@gmail.com>
2020-12-11 13:08:51 +0200

    עגר

--- doNet5781_03B_4789_9647/Drivers.cs
+++ doNet5781_03B_4789_9647/Drivers.cs
@@ -315,7 +315,7 @@ namespace doNet5781_03B_4789_9647
              work = 0;
             Time_left = "";
             timeToEndWork = 0;
-
+            //
             Visible = "Hidden";
             InTraveling = false;
             inBreak = false;
