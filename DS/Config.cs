0073381018da5742aaf3409707ce96ae16f1c333
da077 <da077@DESKTOP-HV19KNK>
2020-12-28 00:52:44 +0200

    config

---
+++ DS/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    static class Config
+    {
+        //for Line
+        static int idLineCounter = 0;
+        public static int IdLineCounter => ++idLineCounter;
+
+        //for trip uses
+        static int tripUser = 0;
+        public static int TripUser => ++tripUser;
+
+        //for bus trip
+        static int tripBus = 0;
+        public static int TripBus => ++tripBus;
+
+
+    }
+}
