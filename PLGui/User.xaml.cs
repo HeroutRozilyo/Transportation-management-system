836a4dd17acb5b144022df5c7b506aaf7ceee30e
da077 <da077@DESKTOP-HV19KNK>
2021-01-11 17:35:43 +0200

    ש

--- PLGui/User.xaml.cs
+++ PLGui/User.xaml.cs
@@ -27,10 +27,12 @@ namespace PLGui
         private ObservableCollection<BO.Line> line1 = new ObservableCollection<BO.Line>();
         private ObservableCollection<BO.Line> line2 = new ObservableCollection<BO.Line>();
         private ObservableCollection<BO.Line> empty = new ObservableCollection<BO.Line>();
+
         BO.Station stationData1 = new BO.Station();
         private IEnumerable<BO.Line> temp1;
         BO.Station stationData2 = new BO.Station();
         private IEnumerable<BO.Line> temp2;
+
         private ObservableCollection<BO.Station> stations1 = new ObservableCollection<BO.Station>();
         private ObservableCollection<BO.Station> stations2 = new ObservableCollection<BO.Station>();
 
@@ -42,7 +44,9 @@ namespace PLGui
             InitializeComponent();
             
             RefreshStation();
-           
+            RefreshStationall();
+
+
 
 
         }
@@ -79,7 +83,7 @@ namespace PLGui
 
 
         }
-
+   
         private void station1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string a= station1.SelectedItem.ToString();
@@ -134,10 +138,6 @@ namespace PLGui
             
         }
 
-        private void ee_SourceUpdated(object sender, DataTransferEventArgs e)
-        {
-
-        }
 
         private void checkOkey_Click(object sender, RoutedEventArgs e)
         {
@@ -184,5 +184,149 @@ namespace PLGui
         {
 
         }
+
+
+
+
+
+
+
+
+        private ObservableCollection<BO.Station> stations = new ObservableCollection<BO.Station>();
+        private IEnumerable<BO.Line> lineStation;
+        int oldCode;
+        bool add = false;
+        private void RefreshStationall()
+        {
+            stations = ConvertList(bl.GetAllStations());//to make ObservableCollection
+            ListOfStations.ItemsSource = stations;    
+        }
+
+        public void RefreshLineInStation()
+        {
+            // beforAdj.Clear();
+            // afterAdj.Clear();
+
+            StationDataGrid.DataContext = stationData1;
+
+            lineStation = null;
+            if (stationData1 != null)
+            {
+                oldCode = stationData1.Code;
+                lineStation = bl.GetAllLineIndStation(stationData1.Code);
+
+                //  BeforAfter();
+                //Befor.ItemsSource = beforAdj;
+                //After.ItemsSource = afterAdj;
+
+            }
+            LineInStation.ItemsSource = lineStation;
+
+        }
+      
+        private void ListOfStations_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            add = false;
+            var list = (ListView)sender; //to get the line
+            stationData1 = list.SelectedItem as BO.Station;
+            RefreshLineInStation();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            if (numberText != null)
+            {
+                int Sera = Convert.ToInt32(numberText);
+                BO.Station SearchResult = bl.GetStationByCode(Sera);
+                if (SearchResult != null)
+                {
+                    ObservableCollection<BO.Station> a = new ObservableCollection<BO.Station>();
+                    a.Add(SearchResult);
+                    ListOfStations.ItemsSource = a;
+
+                }
+                else
+                {
+                    ListOfStations.ItemsSource = stations;
+                    //NotExist.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void textBoxTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+
+                TextBox text = sender as TextBox;
+                int lineSteation = int.Parse(text.Text);
+                BO.Station SearchResult = bl.GetStationByCode(lineSteation);
+                if (SearchResult != null)
+                {
+                    ObservableCollection<BO.Station> a = new ObservableCollection<BO.Station>();
+                    a.Add(SearchResult);
+                    ListOfStations.ItemsSource = a;
+
+                }
+                else
+                {
+                    ListOfStations.ItemsSource = stations;
+                    //NotExist.Visibility = Visibility.Visible;
+                }
+
+
+            }
+            if (e.Key == Key.Back)
+            {
+               // NotExist.Visibility = Visibility.Hidden;
+                ListOfStations.ItemsSource = stations;
+            }
+        }
+
+        private void textBoxTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.Any(x => Char.IsDigit(x));
+        }
+
+
+
+
+        private void textBoxTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ListOfStations.ItemsSource = stations;
+            textBoxTextBox.Text = null;
+            ListOfStations.SelectedIndex = -1;
+            stationData1 = null;
+            RefreshLineInStation();
+
+        }
+       string numberText;
+
+        private void textBoxTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (textBoxTextBox.Text != "Search Station here...." && textBoxTextBox.Text != "")
+                numberText = textBoxTextBox.Text;
+            textBoxTextBox.Text = "Search Station here....";
+        }
+
+
+
+
+        private void ListOfStations_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
