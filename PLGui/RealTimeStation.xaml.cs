526dedf9bbee98a71746383736461e37adfce15e
da077 <da077@DESKTOP-HV19KNK>
2021-01-17 22:33:18 +0200

    aa

--- PLGui/RealTimeStation.xaml.cs
+++ PLGui/RealTimeStation.xaml.cs
@@ -84,7 +84,7 @@ namespace PLGui
             };
             timerWorker.WorkerReportsProgress = true;
             timerWorker.WorkerSupportsCancellation = true;
-
+            //
         }
         //
         /// <summary>
