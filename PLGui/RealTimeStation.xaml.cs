f729079d99a8ed6c7f56b8d0df68f48bdf51fc22
heroot12@gmail.com <heroot12@gmail.com>
2021-01-17 18:09:54 +0200

    הכ

---
+++ PLGui/RealTimeStation.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PLGui
+{
+    /// <summary>
+    /// Interaction logic for RealTimeStation.xaml
+    /// </summary>
+    public partial class RealTimeStation : Window
+    {
+        public RealTimeStation()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
